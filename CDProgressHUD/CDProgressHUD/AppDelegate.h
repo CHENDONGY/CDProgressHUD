eabc2fe7ee24954b64b1a962ae3216dcaaba8a96
try_cd_chen <18280087961@163.com>
2017-03-07 11:29:55 +0800

    add code

---
+++ CDProgressHUD/CDProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CDProgressHUD
+//
+//  Created by tryios on 2017/3/7.
+//  Copyright © 2017年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CDProgressHUD/CDProgressHUD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CDProgressHUD
+//
+//  Created by tryios on 2017/3/7.
+//  Copyright © 2017年 chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
